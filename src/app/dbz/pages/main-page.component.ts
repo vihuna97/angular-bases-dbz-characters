ffdb9cae941639ea3218f2f21f6d898182c2b99b
Hugo <hugonavas97@gmail.com>
2023-05-30 20:38:15 -0600

    algo

---
+++ src/app/dbz/pages/main-page.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'main-page-app',
+    templateUrl: './main-page.component.html'
+})
+
+export class MainPageComponent {
+
+}
